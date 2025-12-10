7da2b8844d8301d2123a8aa6a3079c80b0ff3a4d
Mitya <mityapresnyakov@yandex.ru>
2025-12-10 15:57:46 +0300

    сonnecting database

---
+++ backend/app/database.py
@@ -0,0 +1,30 @@
+﻿from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    AsyncSession,
+)
+from typing import Annotated, AsyncGenerator
+from fastapi import Depends
+
+DATABASE_URL = (
+    "postgresql+asyncpg://postgres:password@localhost:5432/news_service"
+)
+
+engine = create_async_engine(DATABASE_URL)
+AsyncSessionLocal = async_sessionmaker(bind=engine)
+Base = declarative_base()
+
+
+async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        except Exception as e:
+            print(e)
+            await session.rollback()
+        finally:
+            await session.close()
+
+
+SessionDep = Annotated[AsyncSession, Depends(get_db_session)]
